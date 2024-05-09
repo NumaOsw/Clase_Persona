34bce4518c365cf62bb45e6f79d2dbdc071f33aa
Licenciaturas <Licenciaturas@L301-M13>
2024-05-09 10:50:55 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Guardado");
+         
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+
+    class Personas
+    {
+        protected string Nombre;
+        protected string Edad;
+        protected string Fecha;
+        protected string Mtrla;
+        protected string Carra;
+        protected string Sld;
+        protected string Contenido;
+        
+
+        public string nombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+        public string edad
+        {
+            get { return Edad; }
+            set { Edad = value; }
+        }
+        public string fecha
+        {
+            get { return Fecha; }
+            set { Fecha = value; }
+        }
+        public string mTrla
+        {
+            get { return Mtrla; }
+            set { Mtrla = value; }
+        }
+        public string caRra
+        {
+            get { return Carra; }
+            set { Carra = value; }
+        }
+        public string sLd
+        {
+            get { return Sld; }
+            set { Sld = value; }
+        }
+        public string cont
+        {
+            get { return Contenido; }
+            set { Contenido = value; }
+        }
+
+        class Alumnos : Personas
+        {
+            public string Personas(string Name, string Fec, string Age, string Raiz, string Place)
+            {
+                Nombre = Name;
+                Edad = Age;
+                Fecha = Fec;
+                Mtrla = Raiz;
+                Carra = Place;
+                
+                return Contenido = ( Name + Age + Fec + Raiz + Place  );
+            }
+
+        }
+        class Empleado : Personas
+        {
+            public string Personas(string Name, string Fec, string Age, string Mnx)
+            {
+                Nombre = Name;
+                Edad = Age;
+                Fecha = Fec;
+               
+                Sld = Mnx;
+                return Contenido = (Name + Age + Fec + Mnx);
+            }
+
+        }
+        class Docente : Personas
+        {
+            public string Personas(string Name, string Fec, string Age, string Raiz, string Mnx)
+            {
+                Nombre = Name;
+                Edad = Age;
+                Fecha = Fec;
+                Mtrla = Raiz;
+                Sld = Mnx;
+                return Contenido = (Name + Age + Fec + Raiz + Mnx);
+            }
+
+        }
+        static class Program
+        {
+
+            /// <summary>
+            /// Punto de entrada principal para la aplicación.
+            /// </summary>
+            [STAThread]
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+
+            }
+        }
+    }
+}
